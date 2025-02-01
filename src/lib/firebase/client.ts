43c2fdb61fc0599d3ba6a2934975e5a2d6f127d1
NMJ2002 <nimsarajayarathna1@gmail.com>
2025-02-01 11:41:00 +0530

    new firebase api

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,16 +1,18 @@
 import { initializeApp } from 'firebase/app';
-import { getDatabase, ref, onValue } from 'firebase/database';
+import { getDatabase } from 'firebase/database';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLBEqsEJiDWxVoRD0kcjeGbJGEtil-7yY",
-  authDomain: "pantryplus-b207e.firebaseapp.com",
-  databaseURL: "https://pantryplus-b207e-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "pantryplus-b207e",
-  storageBucket: "pantryplus-b207e.firebasestorage.app",
-  messagingSenderId: "547753572896",
-  appId: "1:547753572896:web:8a55c5ced38feab04c5a45",
-  measurementId: "G-DN9C2QWLT6"
+  apiKey: "AIzaSyC2oC6TPn6llqTCEDy_J2ce4IcjiQvchRA",
+  authDomain: "pantryplus-88853.firebaseapp.com",
+  databaseURL: "https://pantryplus-88853-default-rtdb.firebaseio.com",
+  projectId: "pantryplus-88853",
+  storageBucket: "pantryplus-88853.firebasestorage.app",
+  messagingSenderId: "1079604597841",
+  appId: "1:1079604597841:web:e79731ff66b1184c5c9470",
+  measurementId: "G-6QTE23CWLM"
 };
 
 const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const db = getDatabase(app);
